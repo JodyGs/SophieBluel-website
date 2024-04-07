7199c04217afde55b9b36c929e4d12729a44a367
Jody Gs <jodygs@Jodys-MacBook-Pro.local>
2024-04-07 08:44:06 +0200

    Display gallery, filters

--- FrontEnd/src/gallery.js
+++ FrontEnd/src/gallery.js
@@ -1,1 +1,86 @@
-console.log("hello Gallery");
+const apiBaseUrl = "http://localhost:5678/api/"
+const galleryHtmlDiv = document.querySelector('.gallery')
+const filtersHtmlDiv = document.querySelector('.filters')
+let galleryData
+let categories
+
+function callAPI(path) {
+  fetch(`${apiBaseUrl}${path}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Erreur lors de la requête à l'API`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      displayGallery(data)
+      getSingleCategoriesFromWorks(data)
+    })
+    .catch(error => {
+      console.error(`Erreur lors de la récupération des données de l'API :, ${error}`);
+    });
+}
+window.onload = callAPI("works")
+
+function displayGallery(data) {
+  const fragment = document.createDocumentFragment();
+  data.forEach(el => {
+    const figure = document.createElement('figure');
+    figure.classList.add('workCard');
+    figure.dataset.category = el.category.name;
+    figure.innerHTML = `
+      <img src="${el.imageUrl}" alt="${el.title}" />
+      <figcaption>${el.title}</figcaption>
+    `;
+    fragment.appendChild(figure);
+  });
+  galleryHtmlDiv.appendChild(fragment);
+}
+
+function getSingleCategoriesFromWorks(data) {
+  let listOfCategories = new Set(); // 
+  data.forEach((el) => {
+    listOfCategories.add(JSON.stringify(el.category));
+  });
+  const arrayOfStrings = [...listOfCategories];
+  categories = arrayOfStrings.map((string) => JSON.parse(string));
+
+  categories.unshift({ id: 0, name: "Tous" })
+
+  displayFiltersButton(categories)
+}
+
+
+function displayFiltersButton(categories) {
+  categories.forEach((element, index) => {
+    const button = document.createElement("button");
+    button.innerText = element.name;
+    button.className = "category";
+    button.classList.toggle("active", index === 0);
+    button.dataset.category = element.name;
+    filtersHtmlDiv.appendChild(button);
+
+    button.addEventListener("click", () => {
+      document.querySelectorAll('.category').forEach(button => {
+        button.classList.remove('active');
+      });
+      button.classList.add('active');
+      toggleWorks(button.dataset.category)
+    });
+  });
+}
+
+function toggleWorks(datasetCategory) {
+  const figures = document.querySelectorAll(".workCard");
+  if ("Tous" === datasetCategory) {
+    figures.forEach((figure) => {
+      figure.style.display = "block";
+    });
+  } else {
+    figures.forEach((figure) => {
+      figure.dataset.category === datasetCategory
+        ? (figure.style.display = "block")
+        : (figure.style.display = "none");
+    });
+  }
+}
